74b2184896e11ed2230f778ea8d88e8e5f1d3212
Roque Lopez <rlopezc27@gmail.com>
2022-08-17 17:46:35 -0400

    Use a model to identify actions and tools

--- tim_reasoning/recipe_tagger.py
+++ tim_reasoning/recipe_tagger.py
@@ -2,7 +2,7 @@ import re
 import torch
 import string
 from transformers import AutoTokenizer, AutoModelForTokenClassification
-from allennlp.predictors.predictor import Predictor
+from os.path import join
 from datasets import ClassLabel
 from itertools import groupby
 from spacy import displacy
@@ -12,45 +12,22 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 punctuation_marks = string.punctuation
 
 
-class RecipeTagger:
-    def __init__(self, ner_model_path, srl_model_path):
-        self.__tokenizer = AutoTokenizer.from_pretrained(ner_model_path)
-        self.__model = AutoModelForTokenClassification.from_pretrained(ner_model_path)
-        label_names = ['O', 'I-QUANTITY', 'I-UNIT', 'I-STATE', 'I-NAME', 'I-DF', 'I-SIZE', 'I-TEMP']
-        self.__labels = ClassLabel(num_classes=len(label_names), names=label_names)
-        self.__predictor = Predictor.from_path(srl_model_path)
+class HuggingFaceModel:
 
-    def predict_entities(self, text, replace_original_tags=True):
-        inputs = self.__tokenizer(text, return_tensors='pt')
+    def __init__(self, model_path, label_names):
+        self.__tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.__model = AutoModelForTokenClassification.from_pretrained(model_path)
+        self.__labels = ClassLabel(num_classes=len(label_names), names=label_names)
+
+    def predict(self, sentence):
+        inputs = self.__tokenizer(sentence, return_tensors='pt')
         tokens = [self.__tokenizer.convert_ids_to_tokens(x) for x in inputs['input_ids']]
         preds = [self.__labels.int2str(x) for x in self.__model(**inputs.to(device)).logits.argmax(axis=-1)]
-        tokens, tags = self.align_token_preds(inputs, tokens, preds)[0]
-
-        if replace_original_tags:
-            new_tags = []
-            for tag in tags:
-                if tag in {'I-DF'}:  # Skip these tags
-                    tag = 'O'
-                tag = tag.replace('I-NAME', 'INGREDIENT')
-                tag = tag.replace('I-QUANTITY', 'QUANTITY')
-                tag = tag.replace('I-UNIT', 'UNIT')
-                tag = tag.replace('I-STATE', 'STATE')
-                tag = tag.replace('I-SIZE', 'SIZE')
-                tag = tag.replace('I-TEMP', 'TEMP')
-                new_tags.append(tag)
-            tags = new_tags
-
-        # Use the other model to identify actions
-        # TODO: Fine tune the RoBERTa model to identify actions and utensils
-        action_phrases = self.extract_action_phrases(' '.join(tokens))
-        for action, phrase in action_phrases:
-            for index in range(len(tokens)):
-                if action == tokens[index] and tags[index] == 'O':
-                    tags[index] = 'ACTION'
+        tokens, tags = self._align_token_preds(inputs, tokens, preds)[0]
 
         return tokens, tags
 
-    def align_token_preds(self, inputs, tokens, preds):
+    def _align_token_preds(self, inputs, tokens, preds):
         results = []
         for idx in range(inputs['input_ids'].shape[0]):
             result = []
@@ -61,7 +38,52 @@ class RecipeTagger:
                     tag = group[0][2]
                     result.append((token, tag))
             results.append(list(zip(*result)))
+
         return results
+
+
+class RecipeTagger:
+
+    def __init__(self, model_path):
+        labels_edwardjross = ['O', 'I-QUANTITY', 'I-UNIT', 'I-STATE', 'I-NAME', 'I-DF', 'I-SIZE', 'I-TEMP']
+        labels_flowgraph = ['I-Ac', 'I-Ac2', 'I-Af', 'I-At', 'I-D', 'I-F', 'O', 'I-Q', 'I-Sf', 'I-St', 'I-T']
+        self.model_edwardjross = HuggingFaceModel(join(model_path, 'recipe_edwardjross'), labels_edwardjross)
+        self.model_flowgraph = HuggingFaceModel(join(model_path, 'recipe_flowgraph'), labels_flowgraph)
+
+    def predict_entities(self, text, replace_original_tags=True):
+        tokens, tags = self.model_edwardjross.predict(text)
+        tokens2, tags2 = self.model_flowgraph.predict(text)
+
+        if replace_original_tags:
+            tags = self._replace_tags(tags)
+            tags2 = self._replace_tags(tags2)
+
+        new_tags = []
+        for token, tag in zip(tokens, tags):
+            for to, ta in zip(tokens2, tags2):
+                #print(to, ta)
+                if token == to and tag == 'O' and ta in {'ACTION', 'TOOL'}:
+                    tag = ta
+
+            new_tags.append(tag)
+
+        return tokens, new_tags
+
+    def _replace_tags(self, tags):
+        new_tags = []
+        mappings_edwardjross = {'I-NAME': 'INGREDIENT', 'I-QUANTITY': 'QUANTITY', 'I-UNIT': 'UNIT', 'I-STATE': 'STATE', 'I-SIZE': 'SIZE', 'I-TEMP': 'TEMP'}
+        mappings_flowgraph = {'I-Ac': 'ACTION', 'I-Ac2': 'ACTION', 'I-Af': 'ACTION', 'I-At': 'ACTION', 'I-D': 'DURATION', 'I-F': 'INGREDIENT', 'I-Q': 'QUANTITY', 'I-Sf': 'STATE',  'I-T': 'TOOL'}
+
+        for tag in tags:
+            if tag in {'I-DF', 'I-St'}:  # Skip these tags
+                tag = 'O'
+            elif tag in mappings_edwardjross:
+                tag = mappings_edwardjross[tag]
+            elif tag in mappings_flowgraph:
+                tag = mappings_flowgraph[tag]
+            new_tags.append(tag)
+
+        return new_tags
 
     def plot_entities(self, tokens, tags, display_entities=None):
         entities = []
@@ -80,8 +102,8 @@ class RecipeTagger:
 
         text_info = [{'text': text.rstrip(), 'ents': entities}]
 
-        entity_names = ['INGREDIENT', 'QUANTITY', 'UNIT', 'STATE', 'SIZE', 'TEMP', 'ACTION']
-        colors = ['orange', 'gray', 'pink', 'yellow', 'blue', 'red', 'green']
+        entity_names = ['INGREDIENT', 'QUANTITY', 'UNIT', 'STATE', 'SIZE', 'TEMP', 'ACTION', 'TOOL', 'DURATION']
+        colors = ['orange', 'gray', 'pink', 'yellow', 'blue', 'red', 'green', 'yellow', 'red']
 
         if display_entities is None:
             display_entities = entity_names  # Show all the entities
@@ -100,7 +122,7 @@ class RecipeTagger:
                     action_relations.append((action, None))
                 action = token
                 action_used = False
-            elif tag in {'INGREDIENT', 'UTENSIL'}:
+            elif tag in {'INGREDIENT', 'TOOL'}:
                 object = token.rstrip(punctuation_marks)
                 if action is not None:
                     action_relations.append((action, object))
@@ -110,28 +132,3 @@ class RecipeTagger:
             action_relations.append((action, None))
 
         return action_relations
-
-    def extract_action_phrases(self, sentence):
-        annotations = self.__predictor.predict(sentence=sentence)
-        if len(annotations['verbs']) == 0:
-            annotations = self.__predictor.predict(sentence=self.convert_to_verb(sentence))
-
-        # print(annotations)
-        action_phrases = []
-
-        for verb_info in annotations['verbs']:
-            description = verb_info['description']
-            roles = re.findall('\[[^\]]+\]', description)  # Find all ARGs
-            action = verb_info['verb']
-
-            for role in roles:
-                match = re.match(r'^\[ARG\d+: (.+)\]$', role)
-                if match:
-                    objects_in_action = match.group(1)
-                    action_phrases.append((action, objects_in_action))
-
-        return action_phrases
-
-    def convert_to_verb(self, sentence):
-        new_sentence = 'To ' + sentence
-        return new_sentence
