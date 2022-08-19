db213b3ac14e00a96ed97c0fa732939161d587b8
Roque Lopez <rlopezc27@gmail.com>
2022-08-19 18:18:58 -0400

    Add unit test for rule-based classifier

---
+++ tests/test_rule_based_classifier.py
@@ -0,0 +1,31 @@
+import sys
+import unittest
+from tim_reasoning import RuleBasedClassifier
+
+
+class TestRuleBasedClassifier(unittest.TestCase):
+    MODEL_TAGGER_PATH = '/Users/rlopez/PTG/experiments/models/recipe_tagger'
+
+    @classmethod
+    def setUpClass(cls):
+        cls.classifier = RuleBasedClassifier(cls.MODEL_TAGGER_PATH)
+
+    def test_is_mistake(self):
+        recipe_step = 'Place tortilla on cutting board.'
+        detected_actions = ['Put tortilla', 'Grab tortilla from plate', 'Place tortilla']
+        expected = False
+        actual = self.classifier.is_mistake(recipe_step, detected_actions)
+        self.assertEqual(actual, expected)
+
+        recipe_step = 'Clean the knife by wiping with a paper towel.'
+        detected_actions = ['Put tortilla', 'Grab tortilla from plate', 'Place tortilla']
+        expected = True
+        actual = self.classifier.is_mistake(recipe_step, detected_actions)
+        self.assertEqual(actual, expected)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        TestRuleBasedClassifier.MODEL_TAGGER_PATH = sys.argv.pop()
+        
+    unittest.main()
