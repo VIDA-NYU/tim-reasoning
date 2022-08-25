ea3bf1b7a1d426f2918a7efac9c5609d37436229
Roque Lopez <rlopezc27@gmail.com>
2022-08-25 17:14:28 -0400

    Provide a single action to the rule-based classifier

--- tests/test_rule_based_classifier.py
+++ tests/test_rule_based_classifier.py
@@ -4,7 +4,7 @@ from tim_reasoning import RuleBasedClassifier
 
 
 class TestRuleBasedClassifier(unittest.TestCase):
-    MODEL_TAGGER_PATH = '/Users/rlopez/PTG/experiments/models/recipe_tagger'
+    MODEL_TAGGER_PATH = None
 
     @classmethod
     def setUpClass(cls):
@@ -12,20 +12,22 @@ class TestRuleBasedClassifier(unittest.TestCase):
 
     def test_is_mistake(self):
         recipe_step = 'Place tortilla on cutting board.'
-        detected_actions = ['Put tortilla', 'Grab tortilla from plate', 'Place tortilla']
+        detected_action = 'Put tortilla'
         expected = False
-        actual = self.classifier.is_mistake(recipe_step, detected_actions)
+        actual = self.classifier.is_mistake(recipe_step, detected_action)
         self.assertEqual(actual, expected)
 
         recipe_step = 'Clean the knife by wiping with a paper towel.'
-        detected_actions = ['Put tortilla', 'Grab tortilla from plate', 'Place tortilla']
+        detected_action = 'Put tortilla'
         expected = True
-        actual = self.classifier.is_mistake(recipe_step, detected_actions)
+        actual = self.classifier.is_mistake(recipe_step, detected_action)
         self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         TestRuleBasedClassifier.MODEL_TAGGER_PATH = sys.argv.pop()
+    else:
+        TestRuleBasedClassifier.MODEL_TAGGER_PATH = '/Users/rlopez/PTG/experiments/models/recipe_tagger'
 
     unittest.main()
