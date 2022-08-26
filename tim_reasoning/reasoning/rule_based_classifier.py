592961782ac3fb352d50e16cbc49bb96cec039ae
Roque Lopez <rlopezc27@gmail.com>
2022-08-25 21:44:24 -0400

    Load spacy resource inside the constructor

--- tim_reasoning/reasoning/rule_based_classifier.py
+++ tim_reasoning/reasoning/rule_based_classifier.py
@@ -1,18 +1,17 @@
-from tim_reasoning.reasoning.recipe_tagger import RecipeTagger
 import sys
 import spacy
 import logging
+from tim_reasoning.reasoning.recipe_tagger import RecipeTagger
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s', stream=sys.stdout)
 logger = logging.getLogger(__name__)
-
-nlp = spacy.load('en_core_web_lg')
 
 
 class RuleBasedClassifier:
 
     def __init__(self, model_tagger_path):
         self.recipe_tagger = RecipeTagger(model_tagger_path)
+        self.nlp = spacy.load('en_core_web_lg')
 
     def is_mistake(self, current_step, detected_action, threshold=0.4):
         tokens, tags = self.recipe_tagger.predict_entities(current_step)
@@ -26,10 +25,10 @@ class RuleBasedClassifier:
 
         for step_action, step_object in step_action_pairs:
             step_text = step_action + ' ' + step_object if step_object is not None else step_action
-            text_query = nlp(step_text)
+            text_query = self.nlp(step_text)
             for detected_action, detected_object in detected_action_pairs:
                 detected_text = detected_action + ' ' + detected_object if detected_object is not None else detected_action
-                similarity = text_query.similarity(nlp(detected_text))
+                similarity = text_query.similarity(self.nlp(detected_text))
                 logger.info('Similarity between %s and %s: (%.2f)' % (step_text, detected_text, similarity))
                 if similarity >= threshold:
                     logger.info('It is not a mistake!')
