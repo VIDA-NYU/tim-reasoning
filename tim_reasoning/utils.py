55ae08b02a4570ebda4b18d7f55b8dba4135f6a1
Roque Lopez <rlopezc27@gmail.com>
2022-10-12 10:57:25 -0400

    Find common words

--- tim_reasoning/utils.py
+++ tim_reasoning/utils.py
@@ -24,21 +24,34 @@ def map_entity_labels(entity_labels, detected_entities):
     new_names = {}
 
     for entity_type in entity_types:
-        new_names[entity_type] = []
+        new_names_tmp = set()
         for detected_entity in detected_entities[entity_type]:
             if detected_entity in entity_labels[entity_type]:
-                new_names[entity_type].append(detected_entity)
+                new_names_tmp.add(detected_entity)
             else:
                 min_distance = float('inf')
                 best_label = None
                 for entity_label in entity_labels[entity_type]:
-                    if detected_entity in entity_label or entity_label in detected_entity:
+                    # if detected_entity in entity_label or entity_label in detected_entity:
+                    if has_common_words(detected_entity, entity_label):
                         distance = levenshtein_distance(detected_entity, entity_label)
                         if distance < min_distance:
                             min_distance = distance
                             best_label = entity_label
 
                 if best_label is not None:
-                    new_names[entity_type].append(best_label)
+                    new_names_tmp.add(best_label)
+        new_names[entity_type] = list(new_names_tmp)
 
     return new_names
+
+
+def has_common_words(word1, word2):
+    words1 = set(word1.split())
+    words2 = set(word2.split())
+    common = words1 & words2
+
+    if len(common) > 0:
+        return True
+    else:
+        return False
