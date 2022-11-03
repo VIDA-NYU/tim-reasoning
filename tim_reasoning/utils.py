521e3c627cfb7da140dc1fccd56bb2b96e7b5ef2
Roque Lopez <rlopezc27@gmail.com>
2022-11-03 06:18:17 -0400

    Create emission matrix

--- tim_reasoning/utils.py
+++ tim_reasoning/utils.py
@@ -1,8 +1,10 @@
 import json
+import pandas as pd
 from os.path import join, dirname
 from Levenshtein import distance as levenshtein_distance
 
 RECIPES_PATH = join(dirname(__file__), 'resource', 'mit_recipes')
+ANNOTATED_VIDEOS_PATH = join(dirname(__file__), 'resource', 'annotated_videos')
 
 
 def load_recipe_entity_labels(recipe_id):
@@ -51,3 +53,31 @@ def has_common_words(word1, word2):
         return True
     else:
         return False
+
+
+def create_matrix(recipe_id, normalize=True):
+    # TODO: Add "no action" label
+    annotations = pd.read_csv(join(ANNOTATED_VIDEOS_PATH, f'recipe_{recipe_id}.csv'), keep_default_na=False)
+    annotations = annotations[annotations['step_id'] != 'NA']
+    annotations['narration'] = annotations['narration'].replace(['NA'], 'no action')
+    unique_steps = annotations['step_id'].unique()
+    unique_actions = annotations['narration'].unique()
+    matrix = {}
+
+    for step in unique_steps:
+        matrix[step] = {}
+        for action in unique_actions:
+            matrix[step][action] = 0
+
+    for index, row in annotations.iterrows():
+        action = row['narration']
+        step = row['step_id']
+        duration = row['stop_sec'] - row['start_sec']
+        matrix[step][action] += duration
+
+    if normalize:
+        for step, actions in matrix.items():
+            total_duration = float(sum([x for x in matrix[step].values()]))
+            matrix[step] = {k: v / total_duration for k, v in matrix[step].items()}
+
+    return matrix
