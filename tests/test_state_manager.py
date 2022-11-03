7fb64a88b5933fbdab690b0b5c4aef066ebaf7e4
Roque Lopez <rlopezc27@gmail.com>
2022-11-03 14:34:15 -0400

    Test check status

--- tests/test_state_manager.py
+++ tests/test_state_manager.py
@@ -9,9 +9,7 @@ class TestStateManager(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.state_manager = StateManager(cls.CONFIGS)
-
-    def test_start_steps(self):
-        recipe = {
+        cls.recipe = {
             "name": "Pinwheels",
             "ingredients": [
                 "1 8-inch flour tortilla",
@@ -41,14 +39,22 @@ class TestStateManager(unittest.TestCase):
                 "Place the pinwheels on a plate."
             ]
         }
+
+    def test_start_steps(self):
         expected_step_id = 0
         expected_step_description = 'Place tortilla on cutting board.'
-        step_data = self.state_manager.start_recipe(recipe)
+        step_data = self.state_manager.start_recipe(self.recipe)
         actual_step_description = step_data['step_description']
         self.assertEqual(actual_step_description, expected_step_description)
         actual_step_id = step_data['step_id']
         self.assertEqual(actual_step_id, expected_step_id)
-        
+
+    def test_check_status(self):
+        self.state_manager.start_recipe(self.recipe)
+        detected_actions = [('place tortilla on chopping board', 0.7), ('scoop peanut butter, knife', 0.8), ('spread-on peanut butter, tortilla', 0.8), ('wipe knife', 0.8), ('scoop grape jelly, knife', 0.8)]
+        detected_objects = [{'xyxyn': [0.27401072, 0.43337104, 0.37002426, 0.6817775], 'confidence': 0.79647225, 'class_id': 9, 'label': 'Jar of jelly'}, {'xyxyn': [0.16237056, 0.5151739, 0.28028676, 0.7697359], 'confidence': 0.5592432, 'class_id': 8, 'label': 'Jar of peanut butter'}, {'xyxyn': [0.16281287, 0.5172336, 0.25855953, 0.6642615], 'confidence': 0.32714963, 'class_id': 12, 'label': 'jar lid'}]
+        self.state_manager.check_status(detected_actions, detected_objects)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
