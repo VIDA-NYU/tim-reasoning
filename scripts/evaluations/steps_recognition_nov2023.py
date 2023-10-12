02a15dac260cc85add882658bbdb00aed1a2f3ad
Roque Lopez <rlopezc27@gmail.com>
2023-10-12 17:18:17 -0400

    Use the new format of outputs

--- scripts/evaluations/steps_recognition_nov2023.py
+++ scripts/evaluations/steps_recognition_nov2023.py
@@ -5,29 +5,31 @@ from os.path import join, dirname
 from tim_reasoning import SessionManager
 
 RESULTS_PATH = '/Users/rlopez/PTG/tim-reasoning/scripts/evaluations/resource'
-
-
-with open(join(dirname(__file__), '../data_generator/resource/object_states/pinwheels_outputs.json'), 'r') as fin:
-    perception_outputs = json.load(fin)
-
 recipe_id = 'pinwheels'
 video_name = 'pinwheels_2023.03.30-16.38.48'
-results = {'video': [], 'true_step': [], 'predicted_step': []}
 
+output_path = join(dirname(__file__), f'../data_generator/resource/perception_object_states/{recipe_id}_session.json')
+
+with open(output_path, 'r') as fin:
+    perception_outputs = json.load(fin)
+
+
+results = {'video': [], 'true_step': [], 'predicted_step': []}
 sm = SessionManager()
 
 for perception_output in perception_outputs:
-    actual_step = perception_output['step_id']
+    actual_step = perception_output['session']['step_id']
     print(f'Actual step: {actual_step}')
     sm.handle_message(message=[perception_output])
     fake_output_reasoning = {
         "session_id": 1,
-        "task_id":  "Pinwheels",
+        "task_id":  "pinwheels",
         "step_id": 1,
         "step_status": "IN_PROGRESS",
         "step_description": "wipe off knife with a paper towel",
         "error_status": True,
         "error_description": "Errors detected in the step"
+        ""
     }
     predicted_step = fake_output_reasoning['step_id']
 
