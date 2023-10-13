2ab3b7756baea823df4b55213da88328b83eae57
Roque Lopez <rlopezc27@gmail.com>
2023-10-13 11:10:59 -0400

    Load data from sessions file

--- scripts/evaluations/steps_recognition_nov2023.py
+++ scripts/evaluations/steps_recognition_nov2023.py
@@ -4,38 +4,41 @@ import pandas as pd
 from os.path import join, dirname
 from tim_reasoning import SessionManager
 
-RESULTS_PATH = '/Users/rlopez/PTG/tim-reasoning/scripts/evaluations/resource'
-recipe_id = 'pinwheels'
-video_name = 'pinwheels_2023.03.30-16.38.48'
-
-output_path = join(dirname(__file__), f'../data_generator/resource/perception_object_states/{recipe_id}_session.json')
-
-with open(output_path, 'r') as fin:
-    perception_outputs = json.load(fin)
+RESULTS_PATH = join(dirname(__file__), './resource')
+PERCEPTION_OUTPUTS_PATH = join(dirname(__file__), '../data_generator/resource/perception_object_states')
 
 
-results = {'video': [], 'true_step': [], 'predicted_step': []}
-sm = SessionManager()
+def generate_predictions(file_name):
+    output_path = join(PERCEPTION_OUTPUTS_PATH, f'{file_name}.json')
 
-for perception_output in perception_outputs:
-    actual_step = perception_output['session']['step_id']
-    print(f'Actual step: {actual_step}')
-    sm.handle_message(message=[perception_output])
-    fake_output_reasoning = {
-        "session_id": 1,
-        "task_id":  "pinwheels",
-        "step_id": 1,
-        "step_status": "IN_PROGRESS",
-        "step_description": "wipe off knife with a paper towel",
-        "error_status": True,
-        "error_description": "Errors detected in the step"
-        ""
-    }
-    predicted_step = fake_output_reasoning['step_id']
+    with open(output_path, 'r') as fin:
+        perception_outputs = json.load(fin)
 
-    results['true_step'].append(actual_step)
-    results['predicted_step'].append(predicted_step)
-    results['video'].append(video_name)
+    results = {'video': [], 'true_step': [], 'predicted_step': []}
+    sm = SessionManager()
 
-    results_df = pd.DataFrame.from_dict(results)
-    results_df.to_csv(join(RESULTS_PATH, f'results_{recipe_id}.csv'), index=False)
+    for perception_output in perception_outputs:
+        actual_step = perception_output['session']['step_id']
+        print(f'Actual step: {actual_step}')
+        sm.handle_message(message=[perception_output])
+        fake_output_reasoning = {
+            "session_id": 1,
+            "task_id":  "pinwheels",
+            "step_id": 1,
+            "step_status": "IN_PROGRESS",
+            "step_description": "wipe off knife with a paper towel",
+            "error_status": True,
+            "error_description": "Errors detected in the step"
+            ""
+        }
+        predicted_step = fake_output_reasoning['step_id']
+        predicted_session = fake_output_reasoning['session_id']
+        results['true_step'].append(actual_step)
+        results['predicted_step'].append(predicted_step)
+        results['video'].append(predicted_session)
+        results_df = pd.DataFrame.from_dict(results)
+        results_df.to_csv(join(RESULTS_PATH, f'results_{file_name}.csv'), index=False)
+
+
+perception_outputs_file = 'pinwheels_session'
+generate_predictions(perception_outputs_file)
