0180a2d11b2ce333215a2381872e85b063e3a27a
Roque Lopez <rlopezc27@gmail.com>
2023-10-19 21:51:18 -0400

    Remove unnecessary imports

--- tim_reasoning/utils.py
+++ tim_reasoning/utils.py
@@ -2,7 +2,6 @@ import json
 import pandas as pd
 import numpy as np
 from os.path import join, dirname
-from Levenshtein import distance as levenshtein_distance
 
 RECIPES_PATH = join(dirname(__file__), 'resource', 'mit_recipes')
 ANNOTATED_VIDEOS_PATH = join(dirname(__file__), 'resource', 'annotated_videos')
@@ -16,33 +15,6 @@ def load_recipe_entity_labels(recipe_id):
                                 'tools': list(recipe_data['tool_objects'].keys())}
 
         return recipe_object_labels
-
-
-def map_entity_labels(entity_labels, detected_entities):
-    entity_types = entity_labels.keys()
-    new_names = {}
-
-    for entity_type in entity_types:
-        new_names_tmp = set()
-        for detected_entity in detected_entities[entity_type]:
-            if detected_entity in entity_labels[entity_type]:
-                new_names_tmp.add(detected_entity)
-            else:
-                min_distance = float('inf')
-                best_label = None
-                for entity_label in entity_labels[entity_type]:
-                    # if detected_entity in entity_label or entity_label in detected_entity:
-                    if has_common_words(detected_entity, entity_label):
-                        distance = levenshtein_distance(detected_entity, entity_label)
-                        if distance < min_distance:
-                            min_distance = distance
-                            best_label = entity_label
-
-                if best_label is not None:
-                    new_names_tmp.add(best_label)
-        new_names[entity_type] = list(new_names_tmp)
-
-    return new_names
 
 
 def has_common_words(word1, word2):
